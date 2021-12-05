bb0bfad860845b4b14113a373274d2e3e8f31f48
Ryosuke <ryosuke.san.hana@gmail.com>
2021-12-05 01:49:56 -0800

    :zap: fetch with post

--- packages/frontend/utils/fetch.ts
+++ packages/frontend/utils/fetch.ts
@@ -1,12 +1,22 @@
 import { getUserCookie } from "./cookies";
 
-export const fetchWithAuth = async (url: string) => {
+export const fetchWithAuth = async (url: string, body?: any) => {
   const token = getUserCookie();
   if (token) {
+    const postHeaders = body
+      ? {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        }
+      : {};
+
     const results = await fetch(url, {
+      method: body ? "POST" : "GET",
       headers: {
+        ...postHeaders,
         Authorization: `Bearer ${token}`,
       },
+      body: body ? JSON.stringify(body) : null,
     });
     const data = await results.json();
     return data;
