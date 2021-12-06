1526b236d6ad211a9429ba94018c9b7bf2a8845f
Ryosuke <ryosuke.san.hana@gmail.com>
2021-12-05 23:11:15 -0800

    :zap: added date to header

--- packages/frontend/components/CalendarBrowser/index.tsx
+++ packages/frontend/components/CalendarBrowser/index.tsx
@@ -2,40 +2,44 @@ import { Button, Flex } from "@chakra-ui/react";
 import React, { ReactElement } from "react";
 
 interface Props {
+  currentDate: Date;
   setCurrentDate: any;
 }
 
 export default function CalendarBrowser({
+  currentDate,
   setCurrentDate,
 }: Props): ReactElement {
-  var date = new Date();
-  //   var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-  //   var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  const currentDayIndex = currentDate.getDate();
   const daysInMonth = new Date(
-    date.getFullYear(),
+    currentDate.getFullYear(),
     // We go to next month because...
-    date.getMonth() + 1,
+    currentDate.getMonth() + 1,
     // 0 goes to the last month
     0
   ).getDate();
 
-  console.log("days in month", date.getMonth(), daysInMonth);
+  console.log("days in month", currentDate.getMonth(), daysInMonth);
 
-  // for (var d = new Date(2012, 0, 1); d <= now; d.setDate(d.getDate() + 1)) {
-  //   daysOfYear.push(new Date(d));
-  // }
   const handleDateChange = (newDayNum) => {
-    const newDate = new Date(date.getFullYear(), date.getMonth(), newDayNum);
+    const newDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      newDayNum
+    );
     setCurrentDate(newDate);
   };
 
-  const createDayButtons = new Array(daysInMonth)
-    .fill(0)
-    .map((_, dayIndex) => (
-      <Button onClick={() => handleDateChange(dayIndex + 1)}>
-        {dayIndex + 1}
-      </Button>
-    ));
+  console.log("currentDayIndex", currentDate.toDateString(), currentDayIndex);
+
+  const createDayButtons = new Array(daysInMonth).fill(0).map((_, dayIndex) => (
+    <Button
+      onClick={() => handleDateChange(dayIndex + 1)}
+      fontWeight={currentDayIndex === dayIndex + 1 ? "bold" : "normal"}
+    >
+      {dayIndex + 1}
+    </Button>
+  ));
 
   return (
     <Flex width="100%" overflowX="scroll">
--- packages/frontend/components/Tabs/JournalTab/index.tsx
+++ packages/frontend/components/Tabs/JournalTab/index.tsx
@@ -12,13 +12,27 @@ const DEFAULT_DATE = new Date();
 const JournalTab = (props: Props) => {
   const [currentDate, setCurrentDate] = useState(DEFAULT_DATE);
   console.log("date", currentDate.toDateString());
+  const isToday =
+    currentDate.toISOString().split("T")[0] ===
+    DEFAULT_DATE.toISOString().split("T")[0];
+
+  const currentDateString = isToday
+    ? "Today"
+    : `${currentDate.toLocaleString("default", { month: "long" })} 
+        ${currentDate.getDate()}, ${currentDate.getFullYear()}`;
   return (
     <Box>
+      <Heading size="sm" color="gray.500">
+        {currentDateString}
+      </Heading>
       <Heading>My Journal</Heading>
       {/* CATEGORY TABS */}
       <HabitList date={currentDate} />
       {/* CALENDAR BROWSER */}
-      <CalendarBrowser setCurrentDate={setCurrentDate} />
+      <CalendarBrowser
+        currentDate={currentDate}
+        setCurrentDate={setCurrentDate}
+      />
       {/* NEW HABIT BUTTON */}
     </Box>
   );
